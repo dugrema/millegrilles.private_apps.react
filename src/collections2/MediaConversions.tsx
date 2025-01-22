9309f3677a8b60efc297237aeba469d0735e1916
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-01-22 17:24:49 -0500

    Added navigation to conversion job files.
    Reset job list button.

--- src/collections2/MediaConversions.tsx
+++ src/collections2/MediaConversions.tsx
@@ -11,6 +11,7 @@ import TrashIcon from '../resources/icons/trash-2-svgrepo-com.svg';
 import ActionButton from "../resources/ActionButton";
 import { proxy } from "comlink";
 import { SubscriptionMessage } from "millegrilles.reactdeps.typescript";
+import { useNavigate } from "react-router-dom";
 
 function MediaConversionsPage() {
 
@@ -35,11 +36,23 @@ function MediaConversionsList() {
 
     let workers = useWorkers();
     let ready = useConnectionStore(state=>state.connectionAuthenticated);
+    let navigate = useNavigate();
 
     let currentJobs = useMediaConversionStore(state=>state.currentJobs);
     let removeConversionJobs = useMediaConversionStore(state=>state.removeConversionJobs);
+    let setConversionJobs = useMediaConversionStore(state=>state.setConversionJobs);
+
+    let openFileHandler = useCallback((e: MouseEvent<HTMLDivElement>) => {
+        let tuuid = e.currentTarget.dataset.tuuid;
+        navigate(`/apps/collections2/f/${tuuid}`);
+    }, [navigate]);
+
+    let resetListHandler = useCallback(()=>{
+        setConversionJobs(null);
+    }, [setConversionJobs]);
 
     let removeJobHandler = useCallback(async (e: MouseEvent<HTMLButtonElement>)=>{
+        e.stopPropagation();  // Avoid opening video Link
         if(!workers || !ready || !currentJobs) throw new Error('Workers not initialized or missing context');
         let jobId = e.currentTarget.value;
         let job = currentJobs[jobId];
@@ -57,9 +70,9 @@ function MediaConversionsList() {
     }, [currentJobs]);
 
     let jobsElem = useMemo(()=>{
-        if(!sortedJobs) return [];
+        if(!sortedJobs || sortedJobs.length === 0) return [<p key='nojobs'>No jobs.</p>];
         return sortedJobs.map(item=>{
-            let params = 'defaults';
+            let params = '';
             if(item.params) {
                 if(item.params.defaults !== true) {
                     params = `${item.params.codecVideo}, ${item.params.resolutionVideo}`
@@ -78,7 +91,8 @@ function MediaConversionsList() {
             }
 
             return (
-                <div key={item.job_id} className='grid grid-cols-12 odd:bg-slate-700 even:bg-slate-600 hover:bg-violet-800 odd:bg-opacity-40 even:bg-opacity-40'>
+                <div key={item.job_id} onClick={openFileHandler} data-tuuid={item.tuuid}
+                    className='grid grid-cols-12 odd:bg-slate-700 even:bg-slate-600 hover:bg-violet-800 odd:bg-opacity-40 even:bg-opacity-40 cursor-pointer'>
                     <div className='text-center'>
                         <ActionButton onClick={removeJobHandler} value={item.job_id} varwidth={10} confirm={true}>
                             <img src={TrashIcon} alt="Remove job" className='w-8' />
@@ -112,6 +126,13 @@ function MediaConversionsList() {
 
     return (
         <>
+            <div className='pb-2'>
+                <button onClick={resetListHandler}
+                    className='btn inline-block text-center bg-indigo-800 hover:bg-indigo-600 active:bg-indigo-500 disabled:bg-indigo-900'>
+                        Reload
+                </button>
+            </div>
+
             <div className='grid grid-cols-12 bg-slate-800 text-sm user-select-none px-1 w-full'>
                 <p className='col-span-1'></p>
                 <p className='col-span-6 text-sm'>File name</p>
@@ -145,11 +166,29 @@ function SyncMediaConversions() {
     let workers = useWorkers();
     let ready = useConnectionStore(state=>state.connectionAuthenticated);
     let userId = useUserBrowsingStore(state=>state.userId);
+    let currentJobs = useMediaConversionStore(state=>state.currentJobs);
     let setConversionJobs = useMediaConversionStore(state=>state.setConversionJobs);
     let setFileInfoConversionJobs = useMediaConversionStore(state=>state.setFileInfoConversionJobs);
     let updateConversionJob = useMediaConversionStore(state=>state.updateConversionJob);
     let tuuidsToLoad = useMediaConversionStore(state=>state.tuuidsToLoad);
     let setTuuidsToLoad = useMediaConversionStore(state=>state.setTuuidsToLoad);
+
+    // Mechanism to regularly reload list. Not all events are mapped on the media conversion list, this is the way to catch all changes.
+    let [reloadToggle, setReloadToggle] = useState(false);
+    useEffect(()=>{
+        if(!workers || !ready) return;
+        setReloadToggle(true);
+        let interval = setInterval(()=>setReloadToggle(true), 30_000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [workers, ready, setReloadToggle])
+
+    // Toggles reload on list reset
+    useEffect(()=>{
+        if(!workers || !ready || currentJobs) return;
+        setReloadToggle(true);
+    }, [workers, ready, currentJobs, setReloadToggle]);
     
     let conversionJobsUpdateHandler = useCallback((e: SubscriptionMessage)=>{
         if(!workers || !userId) {
@@ -171,6 +210,7 @@ function SyncMediaConversions() {
         }
 
     }, [workers, userId]);
+
     let conversionJobsUpdateHandlerProxy = useMemo(()=>{
         if(!workers || !ready) return null;
         return proxy(conversionJobsUpdateHandler);
@@ -234,6 +274,17 @@ function SyncMediaConversions() {
         workers.connection.collection2SubscribeMediaConversionEvents(conversionJobsUpdateHandlerProxy)
             .catch(err=>console.error("Error registering listener for media conversion events", err));
 
+        return () => {
+            //TODO Unregister job listener
+            workersInner.connection.collection2UnsubscribeMediaConversionEvents(conversionJobsUpdateHandlerProxyInner)
+                .catch(err=>console.error("Error unregistering listener for media conversion events", err));
+        }
+    }, [workers, ready, setConversionJobs, setTuuidsToLoad, conversionJobsUpdateHandlerProxy]);
+
+    useEffect(()=>{
+        if(!workers || !ready || !reloadToggle) return;
+        setReloadToggle(false);  // Got triggered, prevent new execution until the proper time
+        console.debug("Reload media list");
         workers.connection.collections2GetConversionJobs()
             .then(response=>{
                 if(response.ok === false) throw new Error(response.err);
@@ -246,17 +297,10 @@ function SyncMediaConversions() {
                 else setConversionJobs([]);
             })
             .catch(err=>console.error("Error loading conversion jobs", err));
-
-        return () => {
-            //TODO Unregister job listener
-            workersInner.connection.collection2UnsubscribeMediaConversionEvents(conversionJobsUpdateHandlerProxyInner)
-                .catch(err=>console.error("Error unregistering listener for media conversion events", err));
-        }
-    }, [workers, ready, setConversionJobs, setTuuidsToLoad, conversionJobsUpdateHandlerProxy]);
+    }, [workers, ready, reloadToggle, setReloadToggle]);
 
     return <></>;
 }
-
 
 function sortJobs(a: ConversionJobStoreItem, b: ConversionJobStoreItem): number {
     if(a === b) return 0;
