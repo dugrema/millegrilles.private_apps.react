3672a691b5f8354558c1a6071f1e47ec066eb8fc
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-06-19 10:39:47 -0400

    Formatting conversation history
    Added limit to filter on last 7 days when first showing the screen

--- src/aichat/ChatSummaryHistory.tsx
+++ src/aichat/ChatSummaryHistory.tsx
@@ -59,6 +59,7 @@ function ChatHistoryList() {
     let lastConversationsUpdate = useChatStore(state=>state.lastConversationsUpdate);
 
     let [conversations, setConversations] = useState(null as null | Conversation[]);
+    const [showOlder, setShowOlder] = useState(false);
 
     let deleteConversationHandler = useCallback(async (e: MouseEvent<HTMLButtonElement>)=>{
         let conversationId = e.currentTarget.value;
@@ -91,24 +92,37 @@ function ChatHistoryList() {
     let conversationsElems = useMemo(()=>{
         if(!conversations) return null;
 
-        let sortedConversations = [...conversations];
+        const currentTime = new Date();
+        let dateCutoff = 0;
+        if(!showOlder) {
+            dateCutoff = Math.floor((currentTime.getTime() - 7 * 86_400_000) / 1000);  // 1 week
+        }
+        let sortedConversations = conversations.filter(item=>{
+            if(dateCutoff) return item.conversation_date > dateCutoff;
+            return true;
+        });
         sortedConversations.sort((a: Conversation, b: Conversation)=>{
             return a.conversation_date - b.conversation_date;
         })
         sortedConversations = sortedConversations.reverse();
 
-        let dateLimit = new Date();
-        const currentTime = new Date();
+        let dateLimit = new Date() as Date | null;
+        let dateFormat = '';
+        let fullDate = false;
 
         const items = [] as JSX.Element[];
         for(const conversation of sortedConversations) {
-            if(conversation.conversation_date && new Date(conversation.conversation_date * 1000) < dateLimit) {
+            if(conversation.conversation_date && dateLimit && new Date(conversation.conversation_date * 1000) < dateLimit) {
                 // Inject divider
                 const param = determineTimeLabel(currentTime, conversation.conversation_date);
                 dateLimit = param[1];
+                fullDate = param[2];
                 let label = param[0];
+                dateFormat = fullDate?'yyyy-MM-DD hh:mm':'hh:mm';
                 if(label) {
                     items.push(<div key={label} className='col-span-12 bg-violet-800/25 px-2 mt-2 mb-1'>{label}</div>)
+                } else if(!dateLimit) {
+                    items.push(<div key={label} className='col-span-12 bg-violet-800/25 px-2 mt-2 mb-1'>Older</div>)
                 } else {
                     items.push(<div key={''+dateLimit} className='col-span-12 bg-violet-800/25 px-2 mt-2 mb-1'>
                         <Formatters.FormatterDate value={dateLimit.getTime()/1000} format="yyyy-MM-DD"/>
@@ -117,6 +131,8 @@ function ChatHistoryList() {
             }
 
             const label = conversation.subject || conversation.initial_query || conversation.cle_id;
+            const dateSpan = fullDate?'col-span-3 sm:col-span-2':'col-span-2 sm:col-span-1'
+            const linkSpan = fullDate?'col-span-7 sm:col-span-9':'col-span-8 sm:col-span-10'
             items.push(
                 <Fragment key={conversation.conversation_id}>
                     <div className='col-span-2 sm:col-span-1'>
@@ -125,10 +141,10 @@ function ChatHistoryList() {
                                 <i className='fa fa-remove' />
                         </ActionButton>
                     </div>
-                    <div className='col-span-2 sm:col-span-1'>
-                        <Formatters.FormatterDate value={conversation.conversation_date} format="HH:mm" />
+                    <div className={dateSpan}>
+                        <Formatters.FormatterDate value={conversation.conversation_date} format={dateFormat} />
                     </div>
-                    <div className='text-left col-span-8 sm:col-span-10 text-ellipsis line-clamp-2'>
+                    <div className={`text-left ${linkSpan} text-ellipsis line-clamp-2`}>
                         <Link to={`/apps/aichat/conversation/${conversation.conversation_id}`}>{label}</Link>
                     </div>
                 </Fragment>                
@@ -136,27 +152,45 @@ function ChatHistoryList() {
         }
 
         return items;
-    }, [conversations, deleteConversationHandler]);
+    }, [conversations, deleteConversationHandler, showOlder]);
 
     if(conversations === null) return <p>Loading...</p>;
     if(!conversationsElems) return <p>No conversations yet.</p>;
 
     return (
-        <div className='grid grid-cols-12 gap-y-1'>
-            {conversationsElems}
-        </div>
+        <>
+            <div className='grid grid-cols-12 gap-y-1'>
+                {conversationsElems}
+            </div>
+            {showOlder?<></>:
+                <div className='full text-center pt-4'>
+                    <button onClick={()=>setShowOlder(true)}
+                        className='btn bg-slate-700 hover:bg-slate-600 active:bg-slate-500 disabled:bg-slate-800'>
+                            More
+                    </button>
+                </div>
+            }
+        </>
     );
 }
 
-function determineTimeLabel(now: Date, reference: number): [string | null, Date] {
+function determineTimeLabel(now: Date, reference: number): [string | null, Date | null, boolean] {
     const refDate = new Date(reference * 1000);
     const nowLimit = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     // console.debug("Ref %O, refdate: %O, nowLimit: %O", reference, refDate, nowLimit);
-    if(refDate > nowLimit) return ['Today', nowLimit];
+    if(refDate > nowLimit) return ['Today', nowLimit, false];
 
     const yesterdayLimit = new Date(nowLimit.getTime() - 86_400_000);
-    if(refDate > yesterdayLimit) return ['Yesterday', yesterdayLimit];
-    
+    if(refDate > yesterdayLimit) return ['Yesterday', yesterdayLimit, false];
+
+    const lastWeekLimit = new Date(nowLimit.getTime() - 7 * 86_400_000);
+    if(refDate > lastWeekLimit) return ['Last 7 days', lastWeekLimit, true];
+
+    const lastMonthLimit = new Date(lastWeekLimit.getTime() - 31 * 86_400_000);
+    if(refDate > lastMonthLimit) return ['Last 4 weeks', lastMonthLimit, true];
+
     const dateLimit = new Date(refDate.getFullYear(), refDate.getMonth(), refDate.getDate());
-    return [null, dateLimit];
+    if(refDate > dateLimit) return ['Previous year', dateLimit, true];
+
+    return [null, null, true]
 }
